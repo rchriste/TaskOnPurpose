e447a5ac3d89a6d318a14234cdfba85e69c45fd4
Russ Christensen 🦀 <russ-christensen@live.com>
2026-01-15 14:10:35 -0800

    Removing asking if a task is primary or background

--- console/src/new_time_spent.rs
+++ console/src/new_time_spent.rs
@@ -1,9 +1,8 @@
 use chrono::{DateTime, Utc};
 
 use crate::data_storage::surrealdb_layer::{
-    surreal_in_the_moment_priority::SurrealAction,
-    surreal_item::SurrealUrgency,
-    surreal_time_spent::{SurrealDedication, SurrealWhyInScope},
+    surreal_in_the_moment_priority::SurrealAction, surreal_item::SurrealUrgency,
+    surreal_time_spent::SurrealWhyInScope,
 };
 
 pub(crate) struct NewTimeSpent {
@@ -12,5 +11,4 @@ pub(crate) struct NewTimeSpent {
     pub(crate) why_in_scope: Vec<SurrealWhyInScope>,
     pub(crate) when_started: DateTime<Utc>,
     pub(crate) when_stopped: DateTime<Utc>,
-    pub(crate) dedication: Option<SurrealDedication>,
 }
